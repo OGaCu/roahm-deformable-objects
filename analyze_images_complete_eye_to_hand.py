ad3162418e16fc6dc82c243f77ba919583d0d7f7
Alex Bowler <albowler@umich.edu>
2026-01-19 22:32:59 -0500

    Update scripts to be more readable and project point onto image

--- analyze_images_complete_eye_to_hand.py
+++ analyze_images_complete_eye_to_hand.py
@@ -1,7 +1,7 @@
 from apriltag_image import apriltag_image
 import cv2
 import numpy as np
-from scipy.spatial.transform import Rotation 
+from scipy.spatial.transform import Rotation
 
 def _skew(w):
     return np.array([
@@ -45,171 +45,113 @@ def _se3_exp(xi, eps=1e-9):
     T[0:3, 3] = p
     return T
 
-detection_transforms = []
-max_images = 30
-for i in range(1, max_images+1):
-    detections = apriltag_image([f"./image_pose_{i}.png"], output_images=True, display_images=True)
-    detection_transforms.append(detections[1])
-print(detection_transforms)
-t_tag_in_cam_frame = []
-r_tag_in_cam_frame = []
-
-for detection in detection_transforms:
-    t_tag_in_cam_frame.append(detection[0:3, 3])
-    r_tag_in_cam_frame.append(detection[0:3, 0:3])
-print(t_tag_in_cam_frame)
-print(r_tag_in_cam_frame)
-
-### Base 2 Gripper transforms
-# achieved_pos1 = np.array([0.56, -0.21, 0.44])
-# achieved_ori1 = np.array([[-0.39569914, 0.04180925, 0.91742802],
-#                           [-0.69671214, 0.63717833, -0.32953904],
-#                           [-0.59834303, -0.76958155, -0.22300191]])
-
-# achieved_pos2 = np.array([0.51, -0.26, 0.5])
-# achieved_ori2 = np.array([[-0.25095132, -0.38018741, 0.890214],
-#                           [-0.8668955, -0.32093063, -0.38143902],
-#                           [0.43071526, -0.86744514, -0.24904479]])
-
-# achieved_pos3 = np.array([0.53, -0.15, 0.55])
-# achieved_ori3 = np.array([[0.11042751, -0.29415084, 0.94935823],
-#                           [-0.48716977, -0.84859911, -0.20626481],
-#                           [0.86629751, -0.43972132, -0.23701008]])
-
-# achieved_pos4 = np.array([0.55, -0.01, 0.59])
-# achieved_ori4 = np.array([[0.12008366, -0.02115112, 0.99253844],
-#                           [-0.02115112, -0.99960059, -0.01874262],
-#                           [0.99253844, -0.01874262, -0.12048306]])
-
-# achieved_pos5 = np.array([0.44, 0.01, 0.66])
-# achieved_ori5 = np.array([[0.02715695, -0.11099294, 0.99345008],
-#                           [-0.05151567, -0.99265144, -0.10949548],
-#                           [0.99830288, -0.04820468, -0.03267526]])
-
-# achieved_pos6 = np.array([0.45, 0.08, 0.57])
-# achieved_ori6 = np.array([[0.12073953, -0.07136262, 0.99011582],
-#                           [0.77001991, -0.62274256, -0.13878416],
-#                           [0.62649126, 0.77916563, -0.020239]])
-
-# achieved_pos7 = np.array([0.51, 0.07, 0.51])
-# achieved_ori7 = np.array([[0.15814902, 0.04933581, 0.98618196],
-#                           [0.9856845, 0.05122055, -0.16063166],
-#                           [-0.05843768, 0.99746801, -0.04052907]])
-
-# achieved_pos8 = np.array([0.56, -0.04, 0.43])
-# achieved_ori8 = np.array([[0.1601518, 0.37584872, 0.91273717],
-#                           [0.7695146, 0.53158963, -0.35392054],
-#                           [-0.6182222, 0.75904559, -0.20408604]])
-
-np_info = np.load("figure_eight_poses.npz")
-positions = []
-orientations = []
-for i in range(max_images):
-    np_stuff = np_info["arr_"+str(i)]
-    positions.append(np_stuff[0:3])
-    orientations.append(Rotation.from_quat(np_stuff[3:]).as_matrix())
-
-print(positions)
-print(orientations)
-# t_base2gripper = [achieved_pos1, achieved_pos2, achieved_pos3, achieved_pos4, achieved_pos5, achieved_pos6, achieved_pos7, achieved_pos8]
-# r_base2gripper = [achieved_ori1, achieved_ori2, achieved_ori3, achieved_ori4, achieved_ori5, achieved_ori6, achieved_ori7, achieved_ori8]
-
-t_base2gripper = positions[0:max_images]
-r_base2gripper = orientations[0:max_images]
-
-# print("t_base2gripper:\n", t_base2gripper)
-# print("r_base2gripper:\n", r_base2gripper)
-
-r_gripper2cam, t_gripper2cam = [], []
-for R, t in zip(r_tag_in_cam_frame, t_tag_in_cam_frame):
-    R_b2g = R.T
-    t_b2g = -R_b2g @ t
-    r_gripper2cam.append(R_b2g)
-    t_gripper2cam.append(t_b2g)
-
-gripper2tag = np.array([[0, 0, -1, 0.0175],
-                        [0, 1, 0, 0], 
-                        [1, 0, 0, 0.062], 
-                        [0, 0, 0, 1]])
-# gripper2tag = np.array([[0, 1, 0, 0],
-#                         [1, 0, 0, 0], 
-#                         [0, 0, -1, 0], 
-#                         [0, 0, 0, 1]])
+def _load_apriltag_transforms(max_images):
+    detection_transforms = []
+    for i in range(1, max_images + 1):
+        detections = apriltag_image([f"./image_pose_{i}.png"], output_images=True, display_images=True)
+        detection_transforms.append(detections[1])
+    return detection_transforms
 
 
-# r_base2tag, t_base2tag = r_base2gripper, t_base2gripper
-r_base2tag, t_base2tag = [], []
-for r, t in zip(r_base2gripper, t_base2gripper):
-    T_base2gripper = np.eye(4)
-    T_base2gripper[0:3, 0:3] = r
-    T_base2gripper[0:3, 3] = t
+def _split_transforms(transforms):
+    t_list = []
+    r_list = []
+    for transform in transforms:
+        t_list.append(transform[0:3, 3])
+        r_list.append(transform[0:3, 0:3])
+    return t_list, r_list
 
 
-    T_gripper2tag = T_base2gripper @ gripper2tag
-    # print(T_gripper2tag)
-    r_base2tag.append(T_gripper2tag[0:3, 0:3])
-    t_base2tag.append(T_gripper2tag[0:3, 3])
-
-# print("t_base2tag:\n", t_base2tag)
-# print("r_base2tag:\n", r_base2tag)
-
-r_tag2base, t_tag2base = [], []
-for R, t in zip(r_base2tag, t_base2tag):
-    T_base2tag = np.eye(4)
-    T_base2tag[0:3, 0:3] = R
-    T_base2tag[0:3, 3] = t
-    T_tag2base = np.linalg.inv(T_base2tag)
-    R_b2t = T_tag2base[0:3, 0:3]
-    t_b2t = T_tag2base[0:3, 3]
-    r_tag2base.append(R_b2t)
-    t_tag2base.append(t_b2t)
+def _load_figure_eight_poses(npz_path, max_images):
+    np_info = np.load(npz_path)
+    positions = []
+    orientations = []
+    for i in range(max_images):
+        np_stuff = np_info["arr_" + str(i)]
+        positions.append(np_stuff[0:3])
+        orientations.append(Rotation.from_quat(np_stuff[3:]).as_matrix())
+    return positions, orientations
 
 
-# print("\n\n\n")
-# print(r_tag2base)
-# print("t tag 2 base:\n", t_tag2base)
-# print("r tag 2 base:\n", r_tag2base)
-# print("cam 2 gripper:\n", t_tag_in_cam_frame)
+def _invert_rt_pairs(r_list, t_list):
+    r_out, t_out = [], []
+    for r, t in zip(r_list, t_list):
+        r_inv = r.T
+        t_inv = -r_inv @ t
+        r_out.append(r_inv)
+        t_out.append(t_inv)
+    return r_out, t_out
 
 
-T_cam2base_list = []
-for index in range(max_images):
-    T_tag2base = np.eye(4)
-    T_tag2base[0:3, 0:3] = r_tag2base[index]
-    T_tag2base[0:3, 3] = t_tag2base[index]
-    T_cam2tag = np.eye(4)
-    T_cam2tag[0:3, 0:3] = r_tag_in_cam_frame[index]
-    T_cam2tag[0:3, 3] = t_tag_in_cam_frame[index]
-    T_cam2base_list.append(T_cam2tag @ T_tag2base)
+def _mean_se3(transforms, max_iters=20, tol=1e-9):
+    t_mean = transforms[0]
+    for _ in range(max_iters):
+        xi_sum = np.zeros(6)
+        for t_i in transforms:
+            xi_sum += _se3_log(np.linalg.inv(t_mean) @ t_i)
+        xi_avg = xi_sum / len(transforms)
+        if np.linalg.norm(xi_avg) < tol:
+            break
+        t_mean = t_mean @ _se3_exp(xi_avg)
+    return t_mean
 
-T_mean = T_cam2base_list[0]
-for _ in range(20):
-    xi_sum = np.zeros(6)
-    for T_i in T_cam2base_list:
-        xi_sum += _se3_log(np.linalg.inv(T_mean) @ T_i)
-    xi_avg = xi_sum / len(T_cam2base_list)
-    if np.linalg.norm(xi_avg) < 1e-9:
-        break
-    T_mean = T_mean @ _se3_exp(xi_avg)
+gripper2tag = np.array([[0, 0, -1, 0.062],
+                            [0, 1, 0, 0],
+                            [1, 0, 0, -0.0175],
+                            [0, 0, 0, 1]])
 
-print("SE3 mean cam 2 base:\n", T_mean)
-# OUTPUT:
-#[[0.0200 0.9997 -0.0131 0.0966]
-#  [0.2746 -0.0180 -0.9614 0.2275]
-#  [-0.9614 0.0156 -0.2749 1.2036]
-#  [0.0000 0.0000 0.0000 1.0000]]
+def main():
+    max_images = 30
+    detection_transforms = _load_apriltag_transforms(max_images)
 
-# print("Single value tag 2 base:\n", T_tag2base)
-# print("Single value cam 2 tag:\n", T_cam2tag)
+    t_tag_in_cam_frame, r_tag_in_cam_frame = _split_transforms(detection_transforms)
 
-# print("Single value cam 2 base:\n", T_cam2base)
+    positions, orientations = _load_figure_eight_poses("figure_eight_poses.npz", max_images)
 
-# R, t = cv2.calibrateHandEye(
-#         R_gripper2base=r_tag2base[:max_images+1],
-#         t_gripper2base=t_tag2base[:max_images+1],
-#         R_target2cam=r_tag_in_cam_frame[:max_images+1], # cam to tag
-#         t_target2cam=t_tag_in_cam_frame[:max_images+1])
-# print("new eye to hand calibration")
-# print("R", R)
-# print("t", t)
-# print("end")
+    t_base2gripper = positions[0:max_images]
+    r_base2gripper = orientations[0:max_images]
+
+    r_gripper2cam, t_gripper2cam = _invert_rt_pairs(r_tag_in_cam_frame, t_tag_in_cam_frame)
+
+
+    r_base2tag, t_base2tag = [], []
+    for r, t in zip(r_base2gripper, t_base2gripper):
+        t_base2gripper_mat = np.eye(4)
+        t_base2gripper_mat[0:3, 0:3] = r
+        t_base2gripper_mat[0:3, 3] = t
+
+        t_base2tag_mat = t_base2gripper_mat @ gripper2tag
+        r_base2tag.append(t_base2tag_mat[0:3, 0:3])
+        t_base2tag.append(t_base2tag_mat[0:3, 3])
+
+    r_tag2base, t_tag2base = [], []
+    for r, t in zip(r_base2tag, t_base2tag):
+        t_base2tag_mat = np.eye(4)
+        t_base2tag_mat[0:3, 0:3] = r
+        t_base2tag_mat[0:3, 3] = t
+        t_tag2base_mat = np.linalg.inv(t_base2tag_mat)
+        r_b2t = t_tag2base_mat[0:3, 0:3]
+        t_b2t = t_tag2base_mat[0:3, 3]
+        r_tag2base.append(r_b2t)
+        t_tag2base.append(t_b2t)
+
+
+    t_cam2base_list = []
+    for index in range(max_images):
+        t_tag2base_mat = np.eye(4)
+        t_tag2base_mat[0:3, 0:3] = r_tag2base[index]
+        t_tag2base_mat[0:3, 3] = t_tag2base[index]
+        t_cam2tag_mat = np.eye(4)
+        t_cam2tag_mat[0:3, 0:3] = r_tag_in_cam_frame[index]
+        t_cam2tag_mat[0:3, 3] = t_tag_in_cam_frame[index]
+        t_cam2base_list.append(t_cam2tag_mat @ t_tag2base_mat)
+
+    print("LAST CAM2BASE")
+    print(t_cam2base_list[-1])
+
+    t_mean = _mean_se3(t_cam2base_list)
+    print("SE3 mean cam 2 base:\n", t_mean)
+
+
+if __name__ == "__main__":
+    main()
--- verify3d2d.py
+++ verify3d2d.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from scipy.spatial.transform import Rotation
 import matplotlib.pyplot as plt
+from analyze_images_complete_eye_to_hand import gripper2tag
 
 # Tag positions and rotations
 def load_saved_transforms(transform_file:str, num_transforms=30):
@@ -14,6 +15,18 @@ def load_saved_transforms(transform_file:str, num_transforms=30):
         rotations.append(Rotation.from_quat(np_stuff[3:]).as_matrix())
     return translations, rotations
 
+def get_center_tag_transforms(t_base2gripper, r_base2gripper):
+    t_base2tag = []
+    r_base2tag = []
+    for r, t in zip(r_base2gripper, t_base2gripper):
+        t_base2gripper_mat = np.eye(4)
+        t_base2gripper_mat[0:3, 0:3] = r
+        t_base2gripper_mat[0:3, 3] = t
+
+        t_base2tag_mat = t_base2gripper_mat @ gripper2tag
+        r_base2tag.append(t_base2tag_mat[0:3, 0:3])
+        t_base2tag.append(t_base2tag_mat[0:3, 3])
+    return t_base2tag, r_base2tag
 
 
 # From prior we know the camera is roughly looking in the negative x axis of the base frame
@@ -57,8 +70,6 @@ def project_3d_to_2d(point_3d, K, T_base2cam, use_cv2=False):
     if not use_cv2:
         point_base_homog = np.hstack([point_3d, 1.0])
         point_cam = (T_base2cam @ point_base_homog)[:3]
-        print(f"Point base: {point_3d}")
-        print(f"Point cam: {point_cam}")
         # Check if point is in front of camera
         if point_cam[2] <= 0:
             print(f"Warning: Point behind camera (z={point_cam[2]})")
@@ -66,7 +77,6 @@ def project_3d_to_2d(point_3d, K, T_base2cam, use_cv2=False):
         # Project using intrinsics
         point_img_homog = K @ point_cam
         point_2d = point_img_homog[:2] / point_img_homog[2]
-        print(f"Point 2D: {point_2d}\n")
     else:
         point_3d = point_3d.reshape(1, 1, 3)
         r_vec, _ = cv2.Rodrigues(T_base2cam[:3, :3])
@@ -74,18 +84,77 @@ def project_3d_to_2d(point_3d, K, T_base2cam, use_cv2=False):
         point_2d, _ = cv2.projectPoints(point_3d, r_vec, t_vec, K, distCoeffs=None)
     return point_2d.reshape(1, 1, 2)
 
+# Plot points 2d with varying color for index
+def plot_2d_points(points_2d):
+    colors = np.linspace(0.0, 1.0, len(points_2d))
+    plt.figure()
+    plt.scatter(points_2d[:, 0], points_2d[:, 1], c=colors, cmap='viridis')
+    plt.colorbar(label='iteration')
+    plt.xlabel('u (px)')
+    plt.ylabel('v (px)')
+    plt.title('Projected 2D points by iteration')
+    plt.gca().invert_yaxis()
+    plt.show()
+
+def get_image_points(max_images, t_base2tag, K, T_base2cam):
+    points_2d = []
+    for i in range(max_images):
+        result = project_3d_to_2d(t_base2tag[i], K, T_base2cam, use_cv2=False)
+        if result is not None:
+            points_2d.append(result)
+    if len(points_2d) == 0:
+        print("No valid projections!")
+        exit()
+    points_2d = np.array([p.reshape(2) for p in points_2d])
+    return points_2d
+
+def project_2d_points_on_images(max_images, points_2d):
+    valid_projections = 0
+    for i in range(1, max_images + 1):
+        img_path = f"./image_pose_{i}.png"
+        img = cv2.imread(img_path)
+        if img is None:
+            print(f"Warning: Could not load {img_path}")
+            continue
+        # Project tag center
+        if i - 1 >= len(points_2d):
+            print(f"Warning: No projection available for image {i}")
+            continue
+        point_2d = points_2d[i - 1]
+        overlay = img.copy()
+        h, w = img.shape[:2]
+        print(f"Image {i}:")
+
+        x_f, y_f = point_2d
+        x = int(round(float(x_f)))
+        y = int(round(float(y_f)))
+        print(f"  ✓ Projected to 2D: ({x}, {y})")
+        # Check if within image bounds
+        if 0 <= x < w and 0 <= y < h:
+            cv2.circle(overlay, (x, y), 8, (0, 255, 255), -1)      # Cyan filled circle
+            cv2.circle(overlay, (x, y), 8, (255, 0, 0), 2)         # Blue outline
+            cv2.putText(overlay, f"({x}, {y})", (x + 10, y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1)
+            print(f"Point is WITHIN image bounds")
+            valid_projections += 1
+        else:
+            print(f"Projected point ({x}, {y}) is OUTSIDE image bounds ({w}x{h})")
+        # Display the image
+        cv2.imshow(f"Image {i} - Projected Tag Center", overlay)
+        key = cv2.waitKey(500)  # 500ms per image
+        if key == ord('q'):
+            print("\nUser interrupted.")
+            break
+    cv2.destroyAllWindows()
+    print(f"\n=== Dry Run Complete ===")
+    print(f"Valid projections: {valid_projections}/{max_images}\n")
+
 # from analyze_images_complete_eye_to_hand import t_base2tag
 # The camera frame in the robot base frame from the calibration done in analyze_images_complete_eye_to_hand
-T_base2cam = np.array([[0.0200, 0.9997, -0.0131, 0.0966], 
-                   [0.2746, -0.0180, -0.9614, 0.2275],
-                   [-0.9614, 0.0156, -0.2749, 1.2036],
-                   [0.0000, 0.0000,  0.0000, 1.0]])
-
-t_base2tag, r_base2tag = load_saved_transforms("figure_eight_poses.npz")
-
-ground_truth_3d_plot(t_base2tag)
-
-
+T_base2cam = np.array([[0.0200, 0.9997, -0.0131, 0.0977],
+ [0.2746, -0.0180, -0.9614, 0.2931],
+ [-0.9614, 0.0156, -0.2749, 1.2668],
+ [0.0000, 0.0000, 0.0000, 1.0000]])
 
 camera_params = [716.3119506835938, 716.3119506835938, 655.386962890625, 397.7469787597656] # fx, fy, cx, cy
 fx = camera_params[0]
@@ -95,75 +164,17 @@ cy = camera_params[3]
 K = np.array([[fx, 0, cx],
               [0, fy, cy],
               [0, 0, 1]])
-
-# do we have to align base and cam frame again?
-R_align = np.array([
-    [ 0,  1,  0],   # X_cam =  Y_base
-    [ 0,  0, -1],   # Y_cam = -Z_base
-    [ 1,  0,  0]    # Z_cam =  X_base
-])
-
 max_images = 29
 
-# def 
-points_2d = []
-for i in range(max_images):
-    result = project_3d_to_2d(t_base2tag[i], K, T_base2cam, use_cv2=False)
-    if result is not None:
-        points_2d.append(result)
-if len(points_2d) == 0:
-    print("No valid projections!")
-    exit()
-points_2d = np.array([p.reshape(2) for p in points_2d])
-# Plot points 2d with varying color for index
-import matplotlib.pyplot as plt
-colors = np.linspace(0.0, 1.0, len(points_2d))
-plt.figure()
-plt.scatter(points_2d[:, 0], points_2d[:, 1], c=colors, cmap='viridis')
-plt.colorbar(label='iteration')
-plt.xlabel('u (px)')
-plt.ylabel('v (px)')
-plt.title('Projected 2D points by iteration')
-plt.gca().invert_yaxis()
-plt.show()
+# Load saved transforms
+t_base2gripper, r_base2gripper = load_saved_transforms("figure_eight_poses.npz")
+t_base2tag, r_base2tag = get_center_tag_transforms(t_base2gripper, r_base2gripper)
 
-# print("=== Dry Run: Projecting Tag Center onto Images ===\n")
-# valid_projections = 0
-# for i in range(1, max_images + 1):
-#     img_path = f"./image_pose_{i}.png"
-#     img = cv2.imread(img_path)
-#     if img is None:
-#         print(f"Warning: Could not load {img_path}")
-#         continue
-#     # Project tag center
-#     point_2d, point_cam = project_3d_to_2d(t_base2tag[i], K, T_mean)
-#     overlay = img.copy()
-#     h, w = img.shape[:2]
-#     print(f"Image {i}:")
-#     print(f"  Tag center in camera frame: {point_cam}")
-#     if point_cam[2] <= 0:
-#         print("tag behind cam")
-#     elif point_2d is None:
-#         print(f"out of bounds")
-#     else:
-#         x, y = point_2d
-#         print(f"  ✓ Projected to 2D: ({x}, {y})")
-#         # Check if within image bounds
-#         if 0 <= x < w and 0 <= y < h:
-#             cv2.circle(overlay, (x, y), 8, (0, 255, 255), -1)      # Cyan filled circle
-#             cv2.circle(overlay, (x, y), 8, (255, 0, 0), 2)         # Blue outline
-#             cv2.putText(overlay, f"({x}, {y})", (x + 10, y - 10),
-#                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1)
-#             print(f"Point is WITHIN image bounds")
-#             valid_projections += 1
-#         else:
-#             print(f"Projected point ({x}, {y}) is OUTSIDE image bounds ({w}x{h})")
-#     # Display the image
-#     cv2.imshow(f"Image {i} - Projected Tag Center", overlay)
-#     key = cv2.waitKey(500)  # 500ms per image
-#     if key == ord('q'):
-#         print("\nUser interrupted.")
-#         break
-# cv2.destroyAllWindows()
-# print(f"\n=== Dry Run Complete ===")
-# print(f"Valid projections: {valid_projections}/{max_images}\n")
+# Plot the 3D positions of the tag locations
+ground_truth_3d_plot(t_base2tag)
+
+# Get and plot 2d projections
+points_2d = get_image_points(max_images, t_base2tag, K, T_base2cam)
+plot_2d_points(points_2d)
+
+project_2d_points_on_images(max_images, points_2d)
